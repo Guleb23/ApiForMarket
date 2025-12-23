5185d31290662f48863a97252c7d8360b12fa347
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-12-24 01:27:42 +0300

    fix

--- ApiForMarket/Services/ShopService/IShopService.cs
+++ ApiForMarket/Services/ShopService/IShopService.cs
@@ -11,12 +11,10 @@ namespace ApiForMarket.Services.ShopService
     {
         public Task<PagedResult<ShortInfoShop>> GetAllShopsAsync(int page, int pageSize);
         public Task<FullShopInfo> GetShopByIdAsync(Guid shopId);
-        public Task<FullShopInfo> GetUserShopByUserId(Guid userId);
+        public Task<FullShopInfo?> GetUserShopByUserId(Guid userId);
         public Task<ShopMessage> CreateShopAsync(CreateShopForService shop, Guid userId);
         public Task<Guid> GetShopIdByUserIdAsync(Guid userId);
-
         public Task<PagedResult<MiddleInfoShop>> GetShopForModeration(int page, int pageSize);
-
         public Task<bool> ModerateShop(Guid shopId, bool result);
     }
 }
